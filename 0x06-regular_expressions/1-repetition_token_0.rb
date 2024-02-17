d2d99b6c2806fee2fed2797feecdfea9898b90d4
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-02-17 03:36:13 -0800

    Tsk 2

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+#Accept one argument from the command line
+input_string = ARGV[0]
+
+#Define the regular expression pattern
+regex_pattern = /hbt{2,5}n/
+
+#Use scan method to match the input pattern against the pattern
+matches = input_string.scan(regex_pattern)
+
+#Output the matched string(s)
+puts matches.join

c187f5f28a7cf93e7b7af37926f5d6af4c7232cf
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-03-24 17:38:22 +0300

    Task 1

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+# Puppet manifest to install Flask from pip3
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
+}
